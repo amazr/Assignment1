3b030c2ea394de7eb981cd490ca82904149132bd
bouldercoder9 <alma9011@colorado.edu>
2019-06-06 19:16:53 -0600

    First commit

--- algC.cpp
+++ algC.cpp
@@ -1,6 +1,37 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main() {
-    
+	bool stop = false;
+	int counter = 0;
+	int sum = 0;
+	int score;
+	string checker;
+
+	cout << "Welcome to Homework Sum!" << endl;
+
+	while (!stop) {
+		counter++;
+		cout << "Enter score " << counter << ": ";
+		cin >> score;
+		sum += score;
+
+		cout << "Do you have more scores to enter? ('y'/'n'): ";
+		cin >> checker;
+
+		if (checker == "n") {
+			stop = true;
+		}
+		cout << endl;
+	}
+
+	cout << endl << "Homework point sum: " << sum << endl;
+
+	if (sum < 150) {
+		cout << "You failed." << endl;
+	}
+	else {
+		cout << "You passed." << endl;
+	}
 }
