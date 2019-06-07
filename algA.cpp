08919ecc6c7c272242c3702e9c39f3df6e245273
bouldercoder9 <alma9011@colorado.edu>
2019-06-06 18:56:41 -0600

    Second commit

--- algA.cpp
+++ algA.cpp
@@ -2,5 +2,26 @@
 using namespace std;
 
 int main() {
+	int numassigned;
+	int sum = 0;
+	int score;
+	cout << "Welcome to Homework Sum!" << endl;
+	cout << "How many homework assignments do you have: ";
+	cin >> numassigned;
+
+	for (int i = 1; i <= numassigned; i++) {
+		cout << "Enter score " << i << ": ";
+		cin >> score;
+		sum += score;
+	}
+
+	cout << endl << "The sum of your homework scores is: " << sum << endl;
+
+	if (sum < 150) {
+		cout << "You Failed." << endl;
+	}
+	else {
+		cout << "You Passed." << endl;
+	}
     
 }
